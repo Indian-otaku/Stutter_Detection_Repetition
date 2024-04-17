07922bce51850ac2c4654724e0204fd7793b8145
Sidharth D <zurajanaikatsurada1222@gmail.com>
2024-04-17 12:01:04 +0530

    Update convert.py

--- utils/convert.py
+++ utils/convert.py
@@ -6,6 +6,8 @@ def convert_bytes_to_file(file_byte_obj : bytes, file_type : str) -> pathlib.Pat
         file_name = pathlib.Path("input_audio.mp3")
     elif file_type == "audio/wav":
         file_name = pathlib.Path("input_audio.wav")
+    elif file_type == "audio/wave":
+        file_name = pathlib.Path("input_audio.wave")
     else:
         return None
     if not os.path.exists(Config.AUDIO_FOLDER_WD):
